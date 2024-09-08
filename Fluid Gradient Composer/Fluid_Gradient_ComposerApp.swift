ba25ae50284d1aef61545af898ee8584b44f106a
samuelhe864 <samuelhe52@outlook.com>
2024-09-08 14:16:06 +0800

    Autosave, persistence for color scheme

--- Fluid Gradient Composer/Fluid_Gradient_ComposerApp.swift
+++ Fluid Gradient Composer/Fluid_Gradient_ComposerApp.swift
@@ -9,9 +9,20 @@ import SwiftUI
 
 @main
 struct Fluid_Gradient_ComposerApp: App {
+    @AppStorage("ColorSchemeMode") private var colorSchemeMode: ColorSchemeMode = .system
+    @Environment(\.colorScheme) private var systemColorScheme
+    var colorScheme: ColorScheme {
+        switch colorSchemeMode {
+        case .light: .light
+        case .dark: .dark
+        case .system: systemColorScheme
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView(configurationStore: .init())
+                .preferredColorScheme(colorScheme)
         }
     }
 }
