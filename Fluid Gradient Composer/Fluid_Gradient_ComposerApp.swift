6587ebdc8be5dd5ec0e3857ef85752a32e18c64a
samuelhe864 <samuelhe52@outlook.com>
2024-09-08 11:26:51 +0800

    Initial Commit

---
+++ Fluid Gradient Composer/Fluid_Gradient_ComposerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fluid_Gradient_ComposerApp.swift
+//  Fluid Gradient Composer
+//
+//  Created by Samuel He on 2024/9/8.
+//
+
+import SwiftUI
+
+@main
+struct Fluid_Gradient_ComposerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fluid Gradient ComposerTests/Fluid_Gradient_ComposerTests.swift
@@ -0,0 +1,16 @@
+//
+//  Fluid_Gradient_ComposerTests.swift
+//  Fluid Gradient ComposerTests
+//
+//  Created by Samuel He on 2024/9/8.
+//
+
+import Testing
+
+struct Fluid_Gradient_ComposerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
