7d8bfcadbfa98a7caf2e6303b115bb839200cce6
samuelhe864 <samuelhe52@outlook.com>
2024-09-08 11:26:51 +0800

    Initial Commit

---
+++ Fluid Gradient Composer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fluid Gradient Composer
+//
+//  Created by Samuel He on 2024/9/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
